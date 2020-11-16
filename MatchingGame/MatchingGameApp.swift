1edecdcde7d6648511cecf2524bbcd87ccc6c6eb
YangZh <yangkazh@gmail.com>
2020-11-16 17:38:12 -0500

    Initial Commit

---
+++ MatchingGame/MatchingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingGameApp.swift
+//  MatchingGame
+//
+//  Created by yang on 11/16/20.
+//
+
+import SwiftUI
+
+@main
+struct MatchingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
